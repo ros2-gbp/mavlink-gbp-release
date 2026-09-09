313f0f9c5b114cfeca2c6397ccf7e000f33520c4
Vladimir Ermakov <vooon341@gmail.com>
2026-09-09 06:58:56 +0000

    Rebase from 'rpm/kilted/mavlink'

--- scripts/format_xml.sh
+++ scripts/format_xml.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
 
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
@@ -9,11 +10,39 @@ mode="format"
 xml_dir="message_definitions"
 keep_old=0
 
-while getopts "h?cd:o" opt; do
+show_help() {
+    cat <<EOF
+Usage: $(basename "$0") [OPTIONS] [xml_file]
+
+Format or check formatting of MAVLink XML definition files using xmllint.
+
+Options:
+  -h        Show this help message and exit.
+  -c        Check mode: report files that need formatting instead of
+            formatting them in place.
+  -d DIR    XML directory to process (default: message_definitions).
+  -o        Keep a .old backup of each file that is reformatted.
+
+If xml_file is given, only that file (relative to DIR) is processed;
+otherwise all *.xml files under DIR are processed.
+EOF
+}
+
+while getopts ":hcd:o" opt; do
     case "$opt" in
-    h|\?)
+    h)
         show_help
         exit 0
+        ;;
+    \?)
+        echo "Error: Invalid option -$OPTARG" >&2
+        show_help
+        exit 1
+        ;;
+    :)
+        echo "Error: Option -$OPTARG requires an argument." >&2
+        show_help
+        exit 1
         ;;
     c)  mode="check"
         ;;
@@ -40,7 +69,7 @@ echo "$xml_files"
 ret=0
 for f in $xml_files
 do
-    xmllint -format "${f}" > "${f}".new
+    xmllint -format "${f}" | sed 's/[[:blank:]]*$//' > "${f}".new
     case "$mode" in
     format)
         if ! cmp "${f}" "${f}".new >/dev/null 2>&1
